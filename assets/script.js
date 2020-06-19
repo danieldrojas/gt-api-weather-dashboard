aab934740b0ba808ccd640e3011d387c109fccee
Daniel Rojas <daniel.drojas@outlook.com>
2020-06-19 09:15:06 -0400

    added localstorage

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
 $(document).ready(function () {
     console.log("inside the function");
+
+    
+    var citiesEntered = localStorage.getItem("citiesEntered")
+
+    if (citiesEntered) {
+        var arrayOfCities = JSON.parse(citiesEntered)
+        // lastCities()
+    } else {
+        var arrayOfCities = []
+    }
+
+  
     
 
-    var currentDay = moment().format('l');
 
-//Filling dates in forecast cards
-    for (var i = 1; i <=5; i++){
-        $("#forecast-date-" + i).text(currentDay)
-    }
 
     function JSONresponse(city) {
 
@@ -23,56 +30,26 @@ $(document).ready(function () {
             // console.log(response)
             var lon = response.coord.lon
             var lat = response.coord.lat
-            var cityName = response.name 
+            var cityName = response.name
             var temp = response.main.temp
             var hum = response.main.humidity
-           
-
-            
 
 
-            
 
-            
-           
+
+
             // var h6El = $("#main-properties");
             function cityProperties() {
-                $(".card-body").append($("<h6>" +  cityName + "</h6>"))
+                $(".card-body").append($("<h6>" + cityName + "</h6>"))
                 $(".card-body").append($("<h6>" + 'Temperature: ' + temp + ' F' + "</h6>"))
                 $(".card-body").append($("<h6>" + 'Humidity: ' + hum + '%' + "</h6>"))
                 $(".card-body").append($("<h6>" + 'Wind Spped: ' + response.wind.speed + ' MPH' + "</h6>"))
-                
-              
-            
+
+
+
             }
-           
-            
-            
 
 
-
-
-
-            // console.log(response.main.temp)
-            // console.log(response.main.humidity)
-            // console.log(response.wind.speed)
-            // var headline = response.response.docs[0].headline.main;
-            // var byline = response.response.docs[0].byline.original
-            // console.log(headline);
-            // console.log(byline);
-
-            // for (var i = 0; i < 5; i++) {
-            //     var headline = response.response.docs[i].headline.main;
-            //     console.log(headline);
-            //     var byline = response.response.docs[i].byline.original
-
-            //     if (byline === null) {
-            //         byline = "The New York Times"
-            //     }
-
-            //     console.log(byline);
-
-            // }
             //40 days forecast
             var dailyForecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely,current&appid=" + APIkey + "&units=imperial"
 
@@ -80,27 +57,27 @@ $(document).ready(function () {
                 url: dailyForecastUrl,
                 method: "get"
             }).then(function (responseForecast) {
-                console.log('this is responseForcast',responseForecast)
+                console.log('this is responseForcast', responseForecast)
 
                 cityProperties()
                 $(".card-body").append($("<h6>" + 'UV Indexs: ' + responseForecast.daily[0].uvi + "</h6>"))
 
-                for (var i = 1; i <= 5; i++){
+                for (var i = 1; i <= 5; i++) {
                     var momentDate = moment().add(i, 'days')
-                    
+
                     console.log('this is our date to putttt', momentDate.format('l'))
-    
+
                     var cardDiv = $('<div>').attr('id', "card-forecast").addClass('card text-white bg-primary')
-                    
+
                     var date = $('<h5>').addClass("card-title")
                     date.attr('id', 'forecast-date-' + i)
                     date.text(momentDate.format('l'))
-                    
+
                     var temp = $('<h5>').addClass("card-title style='margin:20px'")
                     temp.attr('id', 'forecast-temp-' + i)
                     temp.attr('style', 'font-size: .8rem')
                     temp.text("Temp: " + responseForecast.daily[i].temp.day + " °F")
-                    
+
                     var humidity = $('<h5>').addClass("card-title")
                     humidity.attr('id', 'forecast-hum-' + i)
                     humidity.attr('style', 'font-size: .8rem')
@@ -114,7 +91,7 @@ $(document).ready(function () {
 
 
                 }
-               
+
 
 
 
@@ -123,25 +100,50 @@ $(document).ready(function () {
 
         })
 
-    
+
 
 
 
     }
 
 
+
+
+
+
     //events 
-    //Function
     $("#button").on("click", function (event) {
         event.preventDefault();
+        var inputCity = $("#city-input").val()
 
-        var city = $("#city-input").val()
-        JSONresponse(city);
 
+        if (inputCity.length > 0) {
+            arrayOfCities.push(inputCity)
+        } localStorage.setItem('citiesEntered', JSON.stringify(arrayOfCities))
+
+
+
+        // function listOfCities() {
+        var ulEl = $('<ul>').addClass("card list-group list-group-flush")
+        var liEl = $('<li>').addClass("list-group-item")
+        liEl.text(inputCity)
+        ulEl.append(liEl)
+        $("#last-cities").append(ulEl)
+        $("#city-input").val("")
+
+
+
+
+
+        // listOfCities()    
+        JSONresponse(inputCity);
         $(".card-body").empty()
-
-
     })
+
+
+
+
+
 
 
 })
