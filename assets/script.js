97ca12d634cc77fc7b1eb89b7959ead2b2e7697e
Daniel Rojas <daniel.drojas@outlook.com>
2020-06-24 09:13:57 -0400

    added uv background color

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
     var arrayOfCities = []
-  var  swappedCitiesArray =[]
+    var swappedCitiesArray = []
+    var propertiesArray = []
     
     var ulEl = $('<ul>').addClass("card list-group list-group-flush")
 
 
     
     var citiesEntered = localStorage.getItem("citiesEntered")
+    var storedCitiesProperties = localStorage.getItem('citiesProperties')
 
     if (citiesEntered) {
         arrayOfCities = JSON.parse(citiesEntered)
@@ -65,23 +67,30 @@ $(document).ready(function () {
             method: "get"
         }).then(function (response) {
            
+
+            console.log(response)
+          
             var lon = response.coord.lon
             var lat = response.coord.lat
             var cityName = response.name
             var temp = response.main.temp
             var hum = response.main.humidity
+            var windSpeed = response.wind.speed
+
+            propertiesArray = [cityName, temp, hum, windSpeed]
+            localStorage.setItem('propertiesArray', JSON.stringify(propertiesArray))
 
 
 
-
-
-            function cityProperties() {
-                $(".card-body").empty();
-                $(".card-body").append($("<h6>" + cityName + "</h6>"))
-                $(".card-body").append($("<h6>" + 'Temperature: ' + temp + ' F' + "</h6>"))
-                $(".card-body").append($("<h6>" + 'Humidity: ' + hum + '%' + "</h6>"))
-                $(".card-body").append($("<h6>" + 'Wind Spped: ' + response.wind.speed + ' MPH' + "</h6>"))
-            }
+            
+//Adding properties to array 
+          function cityProperties() {
+                    $(".card-body").empty();
+                    $(".card-body").append($("<h6>" + cityName + "</h6>"))
+                    $(".card-body").append($("<h6>" + 'Temperature: ' + temp + ' F' + "</h6>"))
+                    $(".card-body").append($("<h6>" + 'Humidity: ' + hum + '%' + "</h6>"))
+                    $(".card-body").append($("<h6>" + 'Wind Spped: ' + windSpeed + ' MPH' + "</h6>"))
+                }
 
 
          
@@ -93,8 +102,23 @@ $(document).ready(function () {
                 method: "get"
             }).then(function (responseForecast) {
 
+                console.log(responseForecast)
+
+                var uvIndex = responseForecast.daily[0].uvi
+                console.log(uvIndex)
+
                 cityProperties()
-                $(".card-body").append($("<h6>" + 'UV Indexs: ' + responseForecast.daily[0].uvi + "</h6>"))
+
+                if (uvIndex < 3) {
+                    $(".card-body").append($("<h6>" + 'UV Indexs: ' + "<card id='color-uv' style='background-color: green'>" + uvIndex + "</card>" + "</h6> "))
+              } else if (uvIndex < 7) {
+                    $(".card-body").append($("<h6>" + 'UV Indexs: ' + "<card id='color-uv' style='background-color: orange'>" + uvIndex + "</card>" + "</h6> "))
+                } else {
+                    $(".card-body").append($("<h6>" + 'UV Indexs: ' + "<card id='color-uv' style='background-color: red'>" + uvIndex + "</card>" + "</h6> "))
+                }
+
+                // $(".card-body").append($("<h6>" + 'UV Indexs: ' + "<card id='color-uv' style='background-color: red'>" + uvIndex + "</card>" + "</h6> "))
+
 
                 for (var i = 1; i <= 5; i++) {
                     var momentDate = moment().add(i, 'days')
